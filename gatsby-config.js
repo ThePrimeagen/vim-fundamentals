231ca47431f0d870146acd6c2e6249122b9afecb
ThePrimeagen <mpaulson@netflix.com>
2021-02-23 12:50:29 -0700

    try this?

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/lessons`,
+        path: `${__dirname}/course-website/lessons`,
         name: "markdown-pages"
       }
     },
